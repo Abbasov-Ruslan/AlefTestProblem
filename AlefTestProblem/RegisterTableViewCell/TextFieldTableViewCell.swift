666b59897587b4fbe7104ee30e804ce09fb346ae
Ruslan Abbasov <abbasov.ruslan1999@yndex.ru>
2022-10-26 16:28:37 +0400

    create TextFieldTableViewCell

---
+++ AlefTestProblem/RegisterTableViewCell/TextFieldTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TextFieldTableViewCell.swift
+//  AlefTestProblem
+//
+//  Created by Ruslan Abbasov on 26.10.2022.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
