b5c19c0e2604a0ef6e1b2263344ed704bf3e2395
Ruslan Abbasov <abbasov.ruslan1999@yndex.ru>
2022-10-26 12:01:33 +0400

    create tableview programmatically

--- AlefTestProblem/ViewController.swift
+++ AlefTestProblem/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello world \(indexPath.row + 1)"
+        cell.imageView?.image = UIImage(systemName: "house")
+        cell.imageView?.tintColor = .systemBlue
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        return cell
+    }
 
 }
 
